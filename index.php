411b3f7d231895ff7c101ceb40f96afeceeb15ac
haohanhoa-cmd <haohanhoa@gmail.com>
2026-06-25 21:56:29 +0400

    Update index.php

--- index.php
+++ index.php
@@ -312,7 +312,7 @@ async function showIframe(){
             
             iframe.src = blobUrl;
 
-            iframe.setAttribute("sandbox", "allow-scripts allow-same-origin allow-forms");
+            iframe.setAttribute("sandbox","allow-scripts allow-forms allow-same-origin allow-popups allow-downloads allow-modals");
             
             iframe.style.cssText = "width:100%;height:100%;border:0;display:block;";
             iframe.allow = "fullscreen; autoplay; encrypted-media; picture-in-picture";
